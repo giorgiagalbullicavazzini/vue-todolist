51bbf5df88b719f82c9e73ea50b02717d24fdb85
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-15 14:31:58 +0100

    to do list vue connection

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            title: 'La mia To Do List',
+            toDoList: [
+                {
+                    text: 'Andare a fare la spesa',
+                    done: false,
+                }
+            ]
+        }
+    }
+}).mount('#app');
